51c8250fd317f6feaa04273540f000dd90e6eacc
juliantfernandez <julianfernandez@Julians-MacBook-Air.local>
2023-11-01 23:17:43 -0300

    first commit

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/task.dto';
+@Controller('tasks')
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.taskService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() newTask: CreateTaskDto) {
+    return this.taskService.createTask(newTask.title, newTask.description);
+  }
+}
